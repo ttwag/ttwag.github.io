33d1ffa22a494160e9a4a87fb547340a8c3e030d
ttwag <taowang@ucdavis.edu>
2024-09-17 15:17:52 -0700

    Update Website

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu_link");
-    const icon = document.querySelector(".hamburger_icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
